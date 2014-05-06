54859ea758d472f092abc5e8ba557e438788de96
Robert Lord <robert@lord.io>
2014-05-06 14:45:18 -0400

    Add first prototype of game.js

--- tilt/src/game.js
+++ tilt/src/game.js
@@ -5,12 +5,29 @@
     socket.emit('joinRoom', 'controller', roomid);
     this.socket = socket;
     this.callbacks = {};
+    this.socket.on('msg', function() {
+      var a = arguments;
+      var e = a.shift();
+      if (this.callbacks[e] !== undefined) {
+        this.callbacks[e].apply(this, a);
+      }
+    });
   };
 
   Room.prototype = {
-    on: function(event, callback) {
-
-    }
+    on: function(e, callback) {
+      this.callbacks[e] = this.callbacks[e] || [];
+      this.callbacks[e].push(callback);
+      return this;
+    },
+    emit: function() {
+      this.socket.emit.apply(this, arguments);
+    },
+    // off: function(e, callback) {
+    //   this.callbacks[e] = this.callbacks[e] || [];
+    //   this.callbacks[e].push(callback);
+    //   return this;
+    // }
   };
 
   exports.Tilt = exports.Tilt || {};
